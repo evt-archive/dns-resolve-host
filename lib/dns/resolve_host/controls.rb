c5ef157f73b9cafa50fa15f9db13e605d4571585
Nathan Ladd <nathan@obsidianexchange.com>
2016-12-30 10:42:12 -0600

    Control server is loaded only if rubydns can be found in load path

--- lib/dns/resolve_host/controls.rb
+++ lib/dns/resolve_host/controls.rb
@@ -1,5 +1,16 @@
-require 'rubydns'
-
 require 'dns/resolve_host/controls/ip_address'
 require 'dns/resolve_host/controls/hostname'
-require 'dns/resolve_host/controls/server'
+
+begin
+  require 'rubydns'
+  require 'dns/resolve_host/controls/server'
+rescue LoadError
+  require 'pathname'
+
+  log_subject = Pathname.new(__FILE__).relative_path_from(Pathname.new(Dir.pwd))
+  logger = DNS::ResolveHost::Log.get log_subject.to_s
+
+  logger.warn <<~TEXT
+  rubydns is not present in the load path. As a result, the control server was not loaded. If the control server is needed, add `rubydns' to the current project.
+  TEXT
+end
